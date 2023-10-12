30c620e63250a0082ec999e4c9d9d55905be7f42
kuldeep singh <ks.kuldeep.singh689@gmail.com>
2023-10-12 16:24:46 +0530

    feat: login user, access token and refresh token

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import registerUser from "../controllers/auth/registerController.js";
 import verifyUserEmail from "../controllers/auth/verifyEmailController.js";
+import loginUser from "../controllers/auth/loginController.js";
+import { apiLimiter } from "../middlewares/apiLimit.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.get("/verify/:emailToken/:userId", verifyUserEmail);
+router.post("/login", apiLimiter, loginUser);
 
 export default router;
