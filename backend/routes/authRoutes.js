cc53be9ca6e871ef59eef48fbcce280ee08c3492
kuldeep singh <ks.kuldeep.singh689@gmail.com>
2023-10-23 22:00:30 +0530

    feat: add reset password request and reset password

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,6 +5,10 @@ import loginUser from "../controllers/auth/loginController.js";
 import { apiLimiter } from "../middlewares/apiLimit.js";
 import newAccessToken from "../controllers/auth/refreshTokenController.js";
 import { resendVerifyEmailToken } from "../controllers/auth/resendVerifyEmailController.js";
+import {
+  resetPassword,
+  resetPasswordRequest,
+} from "../controllers/auth/passwordResetController.js";
 
 const router = express.Router();
 
@@ -13,5 +17,7 @@ router.get("/verify/:emailToken/:userId", verifyUserEmail);
 router.post("/login", apiLimiter, loginUser);
 router.get("/new_access_token", newAccessToken);
 router.post("/resend_email_token", resendVerifyEmailToken);
+router.post("/reset_password_request", resetPasswordRequest);
+router.post("/reset_password", resetPassword);
 
 export default router;
