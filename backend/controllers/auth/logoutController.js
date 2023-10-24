7b4fee34726449f6c0923166a5410963d6d9a1e4
kuldeep singh <ks.kuldeep.singh689@gmail.com>
2023-10-24 21:54:22 +0530

    feat: add logout user

---
+++ backend/controllers/auth/logoutController.js
@@ -0,0 +1,40 @@
+import asyncHandler from "express-async-handler";
+import User from "../../models/userModel.js";
+
+export const logoutUser = asyncHandler(async (req, res) => {
+  const cookies = req.cookies;
+
+  if (!cookies?.jwt) {
+    res.sendStatus(404);
+    throw new Error("No cookie found");
+  }
+
+  const refreshToken = cookies.jwt;
+
+  const existingUser = await User.findOne({
+    refreshToken,
+  });
+
+  if (!existingUser) {
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    });
+
+    res.sendStatus(204);
+  }
+
+  existingUser.refreshToken = existingUser.refreshToken.filter(
+    (rft) => rft !== refreshToken
+  );
+
+  await existingUser.save();
+
+  res.clearCookie();
+
+  res.status(200).json({
+    success: true,
+    message: `${existingUser.firstName}, you have been logout successfully`,
+  });
+});
